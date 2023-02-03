8fe0f9c95aa980cde66c8e3d9441252ce902a134
Alexandra Ball <alex.ball4000@gmail.com>
2023-02-03 18:21:05 +0400

    less segfault more casting

--- ray_cast.c
+++ ray_cast.c
@@ -6,7 +6,7 @@
 /*   By: aball <aball@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 12:41:46 by ballzball         #+#    #+#             */
-/*   Updated: 2023/02/03 18:03:00 by aball            ###   ########.fr       */
+/*   Updated: 2023/02/03 18:20:14 by aball            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,14 @@ void	cast_horizonal(t_data *data)
 		data->player.ray.hy_inc = -1;
 		data->player.ray.hy = floor(data->player.ray.y) - 1;
 		data->player.ray.hx = (data->player.ray.y - data->player.ray.hy) / tan(degtorad(data->player.ray.angle)) + data->player.ray.x ;
-		data->player.ray.hx_inc = tan(degtorad(data->player.ray.angle));
+		data->player.ray.hx_inc = 1 / tan(degtorad(data->player.ray.angle));
 	}
 	else
 	{
 		data->player.ray.hy_inc = 1;
 		data->player.ray.hy = floor(data->player.ray.y) + 1;
 		data->player.ray.hx = (data->player.ray.y - data->player.ray.hy) / tan(degtorad(data->player.ray.angle)) + data->player.ray.x;
-		data->player.ray.hx_inc = tan(degtorad(data->player.ray.angle));
+		data->player.ray.hx_inc = 1 / tan(degtorad(data->player.ray.angle));
 	}
 }
 
@@ -56,14 +56,14 @@ void	cast_vertical(t_data *data)
 		data->player.ray.vx_inc = -1;
 		data->player.ray.vx = floor(data->player.ray.x) - 1;
 		data->player.ray.vy = ((data->player.ray.x - data->player.ray.vx) / tan(degtorad(data->player.ray.angle))) + data->player.ray.y;
-		data->player.ray.vy_inc = tan(degtorad(data->player.ray.angle));
+		data->player.ray.vy_inc = 1 / tan(degtorad(data->player.ray.angle));
 	}
 	else
 	{
 		data->player.ray.vx_inc = 1;
 		data->player.ray.vx = floor(data->player.ray.x) + 1;
 		data->player.ray.vy = ((data->player.ray.x - data->player.ray.vx) / tan(degtorad(data->player.ray.angle))) + data->player.ray.y;
-		data->player.ray.vy_inc = tan(degtorad(data->player.ray.angle));
+		data->player.ray.vy_inc = 1 / tan(degtorad(data->player.ray.angle));
 	}
 	// printf("tanv: %f\n", tan(degtorad(data->player.ray.angle)));
 }
@@ -136,12 +136,13 @@ void	cast_ray(t_data *data)
 		// printf("horizonal x: %f y: %f\n", data->player.ray.hx / 64, data->player.ray.hy / 64);
 		printf("angle: %f\n", data->player.ray.angle);
 		printf("increase v x: %f y: %f\n", data->player.ray.vx_inc, data->player.ray.vy_inc);
+		printf("increase h x: %f y: %f\n", data->player.ray.hx_inc, data->player.ray.hy_inc);
 		while (check_walls(data) != 0)
 		{
 			printf("hi\n");
 
-		printf("vertical x: %f y: %f\n", data->player.ray.vx, data->player.ray.vy);
-		printf("horizonal x: %f y: %f\n", data->player.ray.hx, data->player.ray.hy);
+		// printf("vertical x: %f y: %f\n", data->player.ray.vx, data->player.ray.vy);
+		// printf("horizonal x: %f y: %f\n", data->player.ray.hx, data->player.ray.hy);
 			// data->player.ray.y += cos(degtorad(data->player.ray.angle));
 			// data->player.ray.x -= sin(degtorad(data->player.ray.angle));
 			// if (data->player.ray.vy > SCREEN_H || data->player.ray.vx > SCREEN_W)
@@ -149,8 +150,8 @@ void	cast_ray(t_data *data)
 			// if (data->player.ray.hy > SCREEN_H || data->player.ray.hx > SCREEN_W)
 			// 	break ;
 			// if ((int)data->player.ray.x % 5 == 0 && (int)data->player.ray.y % 5 == 0)
-			my_mlx_pixel_put(data, data->player.ray.hx * 64, data->player.ray.hy * 64, 0xFFFFFF);
-			my_mlx_pixel_put(data, data->player.ray.vx * 64, data->player.ray.vy * 64, 0x000000);
+			// my_mlx_pixel_put(data, data->player.ray.hx * 64, data->player.ray.hy * 64, 0xFFFFFF);
+			// my_mlx_pixel_put(data, data->player.ray.vx * 64, data->player.ray.vy * 64, 0x000000);
 		}
 		draw_wall_slice(data, rays);
 		rays += 2;
