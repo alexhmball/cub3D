ae59310b86cc67e85c6e4143438efa58acbc1e90
Alexandra Ball <alex.ball4000@gmail.com>
2023-02-14 14:01:00 +0400

    ya

--- move.c
+++ move.c
@@ -1,52 +1,21 @@
 #include "cub3d.h"
 
-int	check_collision(t_data *data, char direction)
+void	check_collision(t_data *data, double x, double y)
 {
-	double	wx;
-	double	wy;
-	double	wangle;
-
-	wx = 0;
-	wy = 0;
-	if (direction == 'S')
+	if (data->map.map[(int)floor(data->player.y_pos)][(int)floor(data->player.x_pos)] == '1')
 	{
-		wangle = degtorad(data->player.degree);
-		wx = data->player.x_pos - cos(wangle) * 0.2;
-		wy = data->player.y_pos + sin(wangle) * 0.2;
+		data->player.x_pos = x;
+		data->player.y_pos = y;
 	}
-	if (direction == 'W')
-	{
-		wangle = degtorad(data->player.degree);
-		wx = data->player.x_pos + cos(wangle) * 0.2;
-		wy = data->player.y_pos - sin(wangle) * 0.2;
-	}
-	if (direction == 'D')
-	{
-		wangle = degtorad(data->player.degree + 90);
-		wx = data->player.x_pos + cos(wangle) * 0.2;
-		wy = data->player.y_pos - sin(wangle) * 0.2;
-	}
-	if (direction == 'A')
-	{
-		wangle = degtorad(data->player.degree - 90);
-		wx = data->player.x_pos + cos(wangle) * 0.2;
-		wy = data->player.y_pos - sin(wangle) * 0.2;
-	}
-	if (wy < 0 || wx < 0)
-		return (1);
-	if ((int)ceil(wy) > two_d_strlen(data->map.map) - 1)
-		return (1);
-	if ((int)ceil(wx) > ft_strlen(data->map.map[(int)ceil(wy)]) - 1)
-		return (1);
-	if (data->map.map[(int)ceil(wy)][(int)ceil(wx)] == 1)
-		return (1);
-	return (0);
 }
 
 void	move_player(t_data *data, char direction)
 {
-	if (check_collision(data, direction))
-		return ;
+	double	prev_x;
+	double	prev_y;
+
+	prev_x = data->player.x_pos;
+	prev_y = data->player.y_pos;
 	if (direction == 'W')
 	{
 		data->player.y_pos -= sin(degtorad(data->player.degree)) * 0.1;
@@ -67,5 +36,5 @@ void	move_player(t_data *data, char direction)
 		data->player.y_pos -= sin(degtorad(check_angle(data->player.degree + 90))) * 0.1;
 		data->player.x_pos += cos(degtorad(check_angle(data->player.degree + 90))) * 0.1;
 	}
-	check_collision(data, direction);
+	check_collision(data, prev_x, prev_y);
 }
